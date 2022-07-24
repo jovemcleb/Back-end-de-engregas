6724a73ac70e6d111fd8c2629327bc934ada9707
Caleb Lima <lima.cardoso.caleb@gmail.com>
2022-07-24 19:57:19 -0300

    :sparkles:feat: adiciona useCase createClient

---
+++ src/modules/clients/useCases/createClientUseCase.ts
@@ -0,0 +1,30 @@
+import { prisma } from "../../../database/prismaClient";
+import { hash } from "bcrypt";
+
+interface ICreateClient {
+  username: string;
+  password: string;
+}
+
+export class CreateClientUseCase {
+  async execute({ username, password }: ICreateClient) {
+    const clientExists = await prisma.clients.findFirst({
+      where: {
+        username: { contains: username },
+      },
+    });
+
+    if (clientExists) {
+      throw new Error("Client already exists");
+    }
+
+    const hashPassword = await hash(password, 10);
+
+    await prisma.clients.create({
+      data: {
+        username,
+        password: hashPassword,
+      },
+    });
+  }
+}
