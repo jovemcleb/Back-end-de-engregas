7b2bc49f8757624fabe7b4b1ccbc4a984e4ac077
Caleb Lima <lima.cardoso.caleb@gmail.com>
2022-09-08 15:37:17 -0300

    :sparkles: feat: adiciona Use case de createDeliveryman

---
+++ src/modules/deliveryman/useCases/createDeliveryman/createDeliverymanUseCase.ts
@@ -0,0 +1,32 @@
+import { hash } from "bcrypt";
+import { prisma } from "../../../../database/prismaClient";
+
+interface ICreateDeliveryman {
+  username: string;
+  password: string;
+}
+
+export class CreateDeliverymanUseCase {
+  async execute({ username, password }: ICreateDeliveryman) {
+    const deliverymanExist = await prisma.deliveryman.findFirst({
+      where: {
+        username: { contains: username },
+      },
+    });
+
+    if (deliverymanExist) {
+      throw new Error("Client already exists");
+    }
+
+    const hashPassword = await hash(password, 10);
+
+    const deliveryman = await prisma.clients.create({
+      data: {
+        username,
+        password: hashPassword,
+      },
+    });
+
+    return deliveryman;
+  }
+}
